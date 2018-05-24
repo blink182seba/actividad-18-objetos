d05af56b186a9948526d704816a2c6a2d1d110fe
Sebastian <blink182seba@gmail.com>
2018-05-24 00:19:30 -0400

    ejercicio terminado

--- ejercicio2/ejercicio2.rb
+++ ejercicio2/ejercicio2.rb
@@ -1,4 +1,23 @@
+require 'date'
 class Course
-  def initialize(name, datestart, datend)
+  def initialize(name, *date)
+    @name = name
+    @date = date.map { |date| Date.parse(date) }
+  end
+  def date_before(f)
+    @date.each do |i|
+      print @name if i < f
+    end
   end
 end
+file = File.open('/Users/rstuardo/Desktop/git/actividad18/ejercicio2/cursos.txt', 'r')
+data = file.readlines
+file.close
+
+cursos = []
+data.each do |line|
+  ls = line.split(', ')
+  cursos << Course.new(*ls)
+
+end
+cursos[0].date_before(Date.parse('2018-07-10'))
